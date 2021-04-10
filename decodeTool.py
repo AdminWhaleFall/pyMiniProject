2f943b79d12fc6390fba7b7a10d77b2080d42d93
WhaleFall <2734184475@qq.com>
2021-04-10 21:56:17 +0800

    修bug

--- decodeTool.py
+++ decodeTool.py
@@ -1,7 +1,7 @@
 '''
 Author: whalefall
 Date: 2021-04-10 21:10:29
-LastEditTime: 2021-04-10 21:33:51
+LastEditTime: 2021-04-10 21:55:26
 Description: 应朋友要求写的一个在`termux`上运行的解编码程序
 '''
 import base64
@@ -19,7 +19,7 @@ def base64Decode(content):
 # print(base64Decode(base64Encode("鲸落")))
 
 def unicodeDecode(content):
-    return content.decode().encode('unicode_escape')
+    return content.encode('utf-8').decode().encode('unicode_escape')
 
 
 def unicodeEecode(content):
@@ -42,19 +42,19 @@ if __name__ == "__main__":
             put = input("请输入功能号:")
             if put == "1":
                 content = input("请输入将要base64加密的字符串:")
-                print("结果:", base64Encode(content))
+                print("结果:%s" % base64Encode(content))
             elif put == "2":
                 content = input("请输入将要base64解密的字符串:")
-                print("结果:", base64Decode(content))
+                print("结果:%s" % base64Decode(content))
             elif put == "3":
                 content = input("请输入将要unicode编码的字符串:")
-                print("结果:", unicodeDecode(content))
+                print("结果:%s" % unicodeEecode(content))
             elif put == "4":
                 content = input("请输入将要unicode解码的字符串:")
-                print("结果:", unicodeEecode(content))
+                print("结果:%s" % unicodeDecode(content))
             else:
                 print("非预期输入!")
         except Exception as e:
             print("未知错误,请检查输入的字符串")
-            
+
             print(traceback.format_exc())
